5d4dfa9e54cf74c6602de94d2a2852534aa53235
Tuner Vocoder <tunervocoder@gmail.com>
2016-09-08 12:12:52 +0000

    adding the ACTUAL Game of life.

--- C/Game of Life/main.c
+++ C/Game of Life/main.c
@@ -1,24 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-char**        inputFile(char *fileName, char (*board)[40])
+void        init(int board[50][50])
 {
-    printf("Processing %s.", fileName);
-    return(board);
+    int     i;
+    int     j;
+    
+    i = 0;
+    j = 0;
+    
+    while(i < 50)
+    {
+        while(j < 50)
+        {
+            board[i][j] = 0;
+            j++;
+        }
+        i++;
+        j = 0;
+    }
 }
 
-int         main(int argc, char **argv)
+void        copy(int board[50][50], int cibleBoard[50][50])
 {
-    char    board[20][40] = {{0}};
+    int     i;
+    int     j;
     
-    if(argc > 20)
+    i = 0;
+    j = 0;
+    
+    while(i < 50)
     {
-        printf("Lines should not exceed 20. \n");
-        return(1);
+        while(j < 50)
+        {
+            cibleBoard[i][j] = board[i][j];
+            j++;
+        }
+        i++;
+        j = 0;
     }
+}
+
+void        display(int board[50][50])
+{
+    int     i;
+    int     j;
     
-    inputFile(argv[1], board);
+    i = 0;
+    j = 0;
     
-    return(0);
+    while(i < 50)
+    {
+        while(j < 50)
+        {
+            if(board[i][j] == 1)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+            j++;
+        }
+        printf("\n");
+        i++;
+        j = 0;
+    }
+}
+
+int         main()
+{
+    int     initBoard[50][50];
+    int     nextBoard[50][50];
+    int     i;
+    int     j;
+    int     sum;
+    
+    i = 1;
+    j = 1;
+    sum = 0;
+    
+    init(initBoard);
+    init(nextBoard);
+    
+    initBoard[25][25] = 1;
+    initBoard[25][26] = 1;
+    initBoard[27][26] = 1;
+    initBoard[26][28] = 1;
+    initBoard[25][29] = 1;
+    initBoard[25][30] = 1;
+    initBoard[25][31] = 1;
+    
+    while(1)
+    {
+        while(i < 49)
+        {
+            while(j < 49)
+            {
+                sum = initBoard[i - 1][j - 1] + initBoard[i - 1][j] + initBoard[i - 1][j + 1] + initBoard[i][j - 1] + initBoard[i][j + 1] + initBoard[i + 1][j - 1] + initBoard[i + 1][j] + initBoard[i + 1][j + 1];
+                nextBoard[i][j] = ((sum == 3) || (sum == 2 && initBoard[i][j] == 1));
+                j++;
+            }
+            j = 1;
+            i++;
+        }
+        i = 1;
+        display(nextBoard);
+        copy(nextBoard, initBoard);
+        usleep(200000);
+    }
 }
